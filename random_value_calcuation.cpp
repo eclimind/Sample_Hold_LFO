917d9dba29f63fcc547ff21cf0b74f153a2dac41
Gzapple <supergodzillapple@gmail.com>
2026-09-23 18:32:22 -0400

    errors in   main, will add code stuff later

--- random_value_calcuation.cpp
+++ random_value_calcuation.cpp
@@ -2,6 +2,36 @@
 #include <iostream>
 using namespace std;
 
+    /*
+    idea for the random lfo gui:
+    one prng with a very long state/period, so long that it looks random. since its deterministic there will be a point that it loops back to the start.
+
+    the second prng will choose a starting point along the first prng so that different instances of the lfo, if chosen, will have a different starting state in the lfo
+
+    this is to make sure multiple instances of the lfo can use the same initial prng while starting at different points in the sequence, so that they dont automatically use the same lfo pattern
+    */
+class mainprng{
+    //oop translation of java into c++;
+    /*
+    private int state;
+    public mainprng(int seed){
+    
+        state = seed;
+    }
+    public int next(){
+        state = ((237 * state) + 248) % (256);
+        return state;
+    }
+    
+    */
+    private: int state;
+    public: 
+        mainprng(int seed){
+        state = (seed);
+    }
+
+}
+
 int main(){
     printf("wassup boss\n");
     std::cout << "hello\n" ;
@@ -15,31 +45,12 @@ int main(){
 
 
     // X(n+1) = (a * X(n) + c) mod m
-    int n = 3;
+    int state = 3;
+    cout << "\n" << "\n" << "\n" << "\n" << "\n" << "\n";
+    double value = static_cast<double>(state) / 256.0;
+    cout << value;
 
-    int a=293482;
-    int c=3;
-    int m=2348;
-
-
-    for(int i = 0;i < 256;i++){
-
-        n = ((a * n) + c) % m;
-
-        printf("%d",n);
-        n++;
-
-    }
-    cout << endl;
-
-    /*
-    idea for the random lfo gui:
-    one prng with a very long state/period, so long that it looks random. since its deterministic there will be a point that it loops back to the start.
-
-    the second prng will choose a starting point along the first prng so that different instances of the lfo, if chosen, will have a different starting state in the lfo
-
-    this is to make sure multiple instances of the lfo can use the same initial prng while starting at different points in the sequence, so that they dont automatically use the same lfo pattern
-    */
+    double phase = 0.0;
 
     return 0;
 }
